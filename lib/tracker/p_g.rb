0f9ab2863228b951b0a34f82dcf985262427a742
Ben Bergstein <bennyjbergstein@gmail.com>
2014-06-19 22:08:36 -0700

    Add recursive story stories sql.

--- lib/tracker/p_g.rb
+++ lib/tracker/p_g.rb
@@ -23,5 +23,17 @@ module Tracker
       @connection = nil
       self.database_url = nil
     end
+
+    def self.recursive_parent_story_stories_sql(story_id)
+      <<-SQL
+WITH RECURSIVE parent_stories(id, parent_story_id, child_story_id) AS (
+  SELECT id, parent_story_id, child_story_id FROM story_stories WHERE child_story_id = #{story_id}
+UNION ALL
+  SELECT ss.id, ss.parent_story_id, ss.child_story_id
+  FROM parent_stories ps, story_stories ss
+  WHERE ps.parent_story_id = ss.child_story_id
+) SELECT id from parent_stories
+SQL
+    end
   end
 end
